65d81ab19ee5cad93e18eb727faae15fbdc47e8c
Hamzeh Abu-Khamseh <87498673+Hamzeh-Abu-Khamseh@users.noreply.github.com>
2025-08-01 11:24:26 +0300

    Add files via upload

---
+++ IComparable Implementation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+public class Person :IComparable<Person>
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int CompareTo(Person other)
+    {
+        if (other == null) return 1;
+        return Age.CompareTo(other.Age);
+    }
+    public Person(string name,int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public override string ToString()
+    {
+        return $"{Name}, {Age} years old";
+    }
+}
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<Person> people = new List<Person>
+        {
+            new Person("Alice", 30),
+            new Person("Bob", 25),
+            new Person("Charlie", 35)
+        };
+        people.Sort(); // Sorts by Age due to IComparable implementation
+        Console.WriteLine("Sorted People by Age:");
+        foreach (var person in people)
+        {
+            Console.WriteLine(person.ToString());
+        }
+        Console.ReadKey();
+    }
+}
---
+++ IList Implementation/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class CustomCollection<T> : IList<T>
+{
+    private List<T> _items = new List<T>();
+
+    public T this[int index]
+    {
+        get => _items[index];
+        set => _items[index] = value;
+    }
+    public int Count => _items.Count;
+    public bool IsReadOnly => false;
+    public void Add(T item) => _items.Add(item);
+    public void Clear() => _items.Clear();
+    public bool Contains(T item) => _items.Contains(item);
+    public void CopyTo(T[] array, int arrayIndex) => _items.CopyTo(array, arrayIndex);
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < _items.Count; i++)
+        {
+            yield return _items[i];
+        }
+    }
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    public int IndexOf(T item) => _items.IndexOf(item);
+    public void Insert(int index, T item) => _items.Insert(index, item);
+    public bool Remove(T item) => _items.Remove(item);
+    public void RemoveAt(int index) => _items.RemoveAt(index);
+
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        CustomCollection<string> myList = new CustomCollection<string>();
+        myList.Add("First");
+        myList.Add("Second");
+        myList.Insert(1, "Inserted");
+
+        Console.WriteLine("Items in the collection:");
+        foreach (var item in myList)
+        {
+            Console.WriteLine(item);
+        }
+
+        myList.RemoveAt(1);
+        Console.WriteLine("\nAfter removing the inserted item:");
+        foreach (var item in myList)
+        {
+            Console.WriteLine(item);
+        }
+
+        myList[2] = "Updated Third";
+    }
+}   
